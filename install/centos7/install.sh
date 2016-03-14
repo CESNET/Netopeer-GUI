a117be3b32c065d5aad983c1e3e5ffc429fdb18f
Tomas Cejka <cejkat@cesnet.cz>
2016-03-14 15:04:45 +0100

    vagrant: prepared and tested Vagrantfile for centos/7

--- install/centos7/install.sh
+++ install/centos7/install.sh
@@ -1,65 +1,61 @@
-sudo yum install -y wget git gcc gcc-c++
+sudo yum install -y wget git gcc gcc-c++ zlib1g-devel libssl-devel openssl-devel cmake pcre-devel autoconf automake json-c-devel
 
-sudo yum install -y zlib1g-devel libssl-devel openssl-devel cmake pcre-devel
+# PHP dependencies:
+yum install -y php php-common php-json php-dom php-pdo php-intl php-sysvsem
+
+(
 wget https://red.libssh.org/attachments/download/177/libssh-0.7.2.tar.xz
 tar -xJf libssh-0.7.2.tar.xz
 mkdir -p libssh-0.7.2/build
 cd libssh-0.7.2/build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DLIB_INSTALL_DIR=/usr/lib64 .. && make -j2 && sudo make install
-cd ../..
+)
 
-git clone https://github.com/CESNET/libyang.git
+(
+git clone -b devel --depth 1 https://github.com/CESNET/libyang.git
 mkdir -p libyang/build
 cd libyang/build
-git checkout devel
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DLIB_INSTALL_DIR=lib64 -DENABLE_BUILD_TESTS=OFF .. && sudo make install && sudo make install
+)
 
-cd ../..
+(
 wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz
 tar -xJf cmocka-1.0.1.tar.xz
 mkdir -p cmocka-1.0.1/build
 cd cmocka-1.0.1/build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr .. && make -j2 && sudo make install
+)
 
-cd ../..
-git clone git://github.com/cejkato2/libwebsockets lws
+(
+git clone --depth 1 git://github.com/cejkato2/libwebsockets lws
 mkdir lws/b
 cd lws/b
-cmake .. && sudo make install
-
+cmake -DCMAKE_INSTALL_PREFIX:PATH=/usr -DLWS_INSTALL_LIB_DIR=/usr/lib -DLIB_SUFFIX=64 .. && sudo make install
+)
 
 # libnetconf2
-# 
-cd ../..
-git clone https://github.com/CESNET/libnetconf2.git
+(
+git clone -b devel --depth 1 https://github.com/CESNET/libnetconf2.git
 mkdir -p libnetconf2/build
 cd libnetconf2/build
-git checkout devel
 cmake -DENABLE_TLS=ON -DENABLE_SSH=ON -DENABLE_BUILD_TESTS=OFF -DCMAKE_INSTALL_PREFIX=/usr -DLIB_INSTALL_DIR=lib64 .. && sudo make install
+)
+ldconfig
 
-
-# libnetconf
-# 
-# cd ../..
-# sudo yum install -y python-setuptools
-# git clone https://github.com/mbj4668/pyang.git
-# cd pyang
-# python setup.py install
-
-# cd ../..
-# git clone --depth 1 https://github.com/CESNET/libnetconf.git
-# sudo yum install -y libxml2-devel libxslt-devel curl-devel libtool
-# cd libnetconf
-# ./configure --prefix=/usr --libdir=/usr/lib64 -q
-
-
-cd ../..
+(
+git clone -b libyang --depth 1 https://github.com/CESNET/mod_netconf
 cd mod_netconf
-# sudo yum install -y json-c-devel
 ./bootstrap.sh
 ./configure
 make
 make install
+)
 
-ldconfig
+(
+cd /var/www/
+git clone -b netopeerguid --depth 1 https://github.com/CESNET/Netopeer-GUI
+cd Netopeer-GUI
+php app/check.php
+php ./composer.phar install
+)
 
